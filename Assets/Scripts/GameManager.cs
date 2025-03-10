1d0b863d5e06021c3314bc74edd30004032dabda
thbarker <thbarker.griz@gmail.com>
2025-03-10 01:02:09 -0700

    Working on gameplay loop

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
 using Unity.Netcode;
-using System.Collections.Generic;
+using System.Collections;
 
 public class GameManager : NetworkBehaviour
 {
     public static GameManager Instance { get; private set; }
+    private PlayerController player1, player2;
 
     public int numberOfPlayers = 0;
+    private bool hasFired = false;
+    private float timeCounter = 0f;
+    private bool gameStarted = false;
+
+    public float roundStartTime = 5f;
+    public float roundTime = 30f;
+    public float roundEndTime = 5f;
 
     void Awake()
     {
@@ -21,27 +29,85 @@ public class GameManager : NetworkBehaviour
         }
     }
 
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F)) 
+        if (!gameStarted && numberOfPlayers == 2) 
         {
-            ChangePlayerModes(Mode.OFFENSE);
+            gameStarted = true;
+            ChangePlayerMode(player1, Mode.OFFENSE);
+            ChangePlayerMode(player2, Mode.DEFENSE);
+            StartCoroutine(GameplayLoop());
         }
     }
-    public void ChangePlayerModes(Mode newMode)
+    public void ChangePlayerMode(PlayerController player, Mode newMode)
     {
-        foreach (var player in NetworkManager.Singleton.ConnectedClientsList)
-        {
-            PlayerController controller = player.PlayerObject.GetComponent<PlayerController>();
-            if (controller != null)
-            {
-                controller.SetPlayerMode(newMode);
-            }
-        }
+        player.SetPlayerMode(newMode);
     }
     public void AddPlayer()
     {
-        numberOfPlayers++;
-        Debug.Log("Player added. Total players: " + numberOfPlayers);
+        if(numberOfPlayers <= 2)
+        {
+            numberOfPlayers++;
+            Debug.Log("Player added. Total players: " + numberOfPlayers);
+        }
+        if (numberOfPlayers == 2)
+        {
+            int index1 = Random.Range(0, 2);
+            int index2 = Mathf.Abs(index1 - 1);
+            player1 = NetworkManager.Singleton.ConnectedClientsList[index1].PlayerObject.GetComponent<PlayerController>();
+            player2 = NetworkManager.Singleton.ConnectedClientsList[index2].PlayerObject.GetComponent<PlayerController>();
+        }
     }
+    public void UpdatePlayerFired(bool flag)
+    {
+        hasFired = flag;
+    }
+
+    private IEnumerator GameplayLoop()
+    {
+        Debug.Log("Round Starting...");
+        // Countdown round start time
+        yield return new WaitForSeconds(roundStartTime);
+
+        // Allow player1 to fire
+        player1.SetCanFire(true);
+
+        // Set initial round time
+        timeCounter = 0; // roundTime should be defined as the duration of the round in seconds
+
+        Debug.Log("Round Going...");
+        // Countdown roundtime if player hasn't shot using hasFired bool
+        while (timeCounter < roundTime && !hasFired)
+        {
+            timeCounter += Time.deltaTime;
+            yield return null;
+        }
+
+        // Round ends either by timer or if player has fired
+        // Update the scoreboard
+
+        // Check who was on offense and switch sides
+        if (player1.mode.Value == Mode.OFFENSE)
+        {
+            ChangePlayerMode(player1, Mode.DEFENSE);
+            ChangePlayerMode(player2, Mode.OFFENSE);
+        }
+        else
+        {
+            ChangePlayerMode(player1, Mode.OFFENSE);
+            ChangePlayerMode(player2, Mode.DEFENSE);
+        }
+
+        // Reset player's fire ability for the next round
+        player1.SetCanFire(false);
+        player2.SetCanFire(false);
+        hasFired = false;
+
+        Debug.Log("Round Ending...");
+        // Optionally, implement a delay or condition before restarting the loop or ending the game
+        yield return new WaitForSeconds(roundEndTime);
+    }
+
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,8 +15,9 @@ public class PlayerController : NetworkBehaviour
     public float speed = 5.0f;
     private Rigidbody rb;
     private Animator animator;
-    private GameObject camera;
+    private GameObject playerCamera;
     public NetworkVariable<Mode> mode = new NetworkVariable<Mode>();
+    public NetworkVariable<bool> canFire = new NetworkVariable<bool>();
     private GameManager gameManager;
     [SerializeField]
     private SkinnedMeshRenderer meshRenderer;
@@ -25,12 +26,12 @@ public class PlayerController : NetworkBehaviour
     {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
-        camera = transform.Find("Camera").gameObject;
+        playerCamera = transform.Find("Camera").gameObject;
         gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
         if (gameManager)
             gameManager.AddPlayer();
         mode.Value = Mode.WAITING;
-        if(IsLocalPlayer)
+        if (IsLocalPlayer)
         {
             meshRenderer.enabled = false;
         }
@@ -49,12 +50,38 @@ public class PlayerController : NetworkBehaviour
         if (IsServer)
         {
             mode.Value = newMode;
-            ResetCameraRotationClientRpc();
+            if(mode.Value == Mode.OFFENSE)
+            {
+                SetOffense();
+            }
+            if (mode.Value == Mode.DEFENSE)
+            {
+                SetDefense();
+            }
+        }
+    }
+
+    private void SetOffense()
+    {
+        ResetPositionServerRpc();
+        ResetCameraRotationClientRpc();
+    }
+    private void SetDefense()
+    {
+        ResetPositionServerRpc();
+        ResetCameraRotationClientRpc();
+    }
+    public void SetCanFire(bool flag)
+    {
+        if (IsServer)
+        {
+           canFire.Value = flag;
         }
     }
 
     private void HandleInput()
     {
+        // Server sided movement
         if (Input.GetKey(KeyCode.A))
         {
             MovePlayerServerRpc(-1);
@@ -68,26 +95,28 @@ public class PlayerController : NetworkBehaviour
             MovePlayerServerRpc(0);
         }
 
-        
-        if (IsLocalPlayer && mode.Value == Mode.OFFENSE) // Ensure this code is only run in Offense mode
+        // Client Sided Aiming
+        AimPlayer();
+
+        // Server Sided Shooting
+        if (Input.GetMouseButtonDown(0))
         {
-            // Handling mouse aiming
-            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-            currentPitch -= mouseY; // Subtracting to invert the vertical input
-            currentPitch = Mathf.Clamp(currentPitch, -90f, 90f); // Clamping pitch to prevent overrotation
-
-            currentYaw += mouseX;
-            Quaternion targetRotation = Quaternion.Euler(currentPitch, currentYaw, 0);
-            camera.transform.localRotation = targetRotation;
+            FireServerRpc();
         }
     }
+
     [ClientRpc]
     private void ResetCameraRotationClientRpc()
     {
         // Resets the camera's rotation relative to the parent object (the player)
-        camera.transform.localRotation = Quaternion.identity;
+        playerCamera.transform.localRotation = Quaternion.identity;
+    }
+    [ServerRpc]
+    private void ResetPositionServerRpc()
+    {
+        if(IsServer)
+            // Resets the players movement to the spawnpoint
+            transform.position = new Vector3(0, transform.position.y, transform.position.z);
     }
 
     [ServerRpc]
@@ -98,6 +127,19 @@ public class PlayerController : NetworkBehaviour
 
         // Set the Speed parameter based on the magnitude of local x velocity
         animator.SetFloat("Speed", localVelocity.x);
+
+        if (mode.Value == Mode.OFFENSE)
+        {
+            animator.SetBool("Aiming", true);
+            animator.SetFloat("AimX", currentYaw / 45);
+            animator.SetFloat("AimY", -currentPitch / 45);
+        }
+        else
+        {
+            animator.SetBool("Aiming", false);
+            animator.SetFloat("AimX", 0);
+            animator.SetFloat("AimY", 0);
+        }
     }
 
     [ServerRpc]
@@ -111,14 +153,76 @@ public class PlayerController : NetworkBehaviour
         }
     }
 
-    [ServerRpc]
-    void FireServerRpc()
+    private void AimPlayer()
     {
-        if (IsServer && mode.Value == Mode.OFFENSE) // Ensure this code is executed on the server
+        if (IsLocalPlayer && mode.Value == Mode.OFFENSE) // Ensure this code is only run in Offense mode
         {
-            
+            // Handling mouse aiming
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            currentPitch -= mouseY; // Subtracting to invert the vertical input
+            currentYaw += mouseX;
+
+            // Clamping the pitch and yaw
+            currentPitch = Mathf.Clamp(currentPitch, -45f, 45f);
+            currentYaw = Mathf.Clamp(currentYaw, -45f, 45f);
+
+            Quaternion targetRotation = Quaternion.Euler(currentPitch, currentYaw, 0);
+            playerCamera.transform.localRotation = targetRotation;
         }
     }
+
+    [ServerRpc]
+    void SetHasFiredTrueServerRpc()
+    {
+        if(IsServer)
+            gameManager.UpdatePlayerFired(true);
+    }
+
+    [ServerRpc(RequireOwnership = true)]
+    void FireServerRpc()
+    {
+        // Check if the player is in OFFENSE mode and allowed to fire
+        if (mode.Value == Mode.OFFENSE && canFire.Value)
+        {
+            // Perform the raycast
+            PerformRaycast();
+            // After firing, set canFire to false to prevent repeated shots without authorization
+            canFire.Value = false;
+            // Inform the GameManager that this player has fired
+            gameManager.UpdatePlayerFired(true);
+        }
+    }
+
+    void PerformRaycast()
+    {
+        if (playerCamera != null)
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+            float maxDistance = 100.0f;  // Define the maximum distance for the raycast
+
+            if (Physics.Raycast(ray, out hit, maxDistance))
+            {
+                // Example: Apply damage if the hit object has a damageable component
+                var damageable = hit.collider.GetComponent<PlayerController>();
+                if (damageable != null)
+                {
+                    Debug.Log("Hit " + hit.collider.name);
+                }
+            }
+            else
+            {
+                Debug.Log("No hit");
+            }
+        }
+        else
+        {
+            Debug.LogError("Camera not found on the player object");
+        }
+    }
+
 
 
 }
